65daebd70d43097ed373d576a6d1061276192115
Sonny Mai <smai@dius.com.au>
2015-11-29 11:50:41 +1100

    aggregate total cups saved

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,8 @@ class DashboardController < ApplicationController
     authorize! :index, :dashboard
     @cafe = User.find(params[:user_id])
     @cup_usages = CupUsage.where(user_id: params[:user_id]).sort(:month => 1)
+
+    @total_cups_saved = @cup_usages.reduce(0) {|sum, cup_usage| sum + cup_usage.reusable_cups + cup_usage.dine_in_cups }
   end
   
 end
